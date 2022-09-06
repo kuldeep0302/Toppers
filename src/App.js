38338578b13cbf2a7aa5c27ad68129f309c7ea19
Kuldeep saini <Kuldeepsaini0302@gmail.com>
2022-09-06 08:44:37 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
+import logo from './logo.svg'
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import Home from './Componants/Home';
+import About from './Componants/About';
+import Navbar from './Componants/Navbar';
 function App() {
   return (
     <div className="App">
-   
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+      <Route path="/Home" element= {<Home/>} /> 
+      <Route path="/About" element= {<About/>} /> 
+     
+      </Routes>
+      </BrowserRouter>
 
 </div>
   );
---
+++ src/componants/Navbar.js
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+function Navbar(){
+    return(
+        <div>
+            <ul>
+                <li>
+                <Link to="/Home" >Home</Link>
+                </li>
+                <li>
+                <Link to="/About" >About</Link>
+                </li>
+            </ul>
+          
+      
+        </div>
+    )
+}
+
+export default Navbar;
